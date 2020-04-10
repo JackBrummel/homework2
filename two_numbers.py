237324a7791cc1a2f92af747359e724cac8c812f
JackBrummel <60793855+JackBrummel@users.noreply.github.com>
2020-04-09 20:46:06 -0700

    Update two_numbers.py

--- two_numbers.py
+++ two_numbers.py
@@ -2,7 +2,3 @@ a = input("enter first number")
 b = input("enter second number")
 if a > b:
     print("a is bigger than b")
-elif a < b:
-    print("b is greater than a")
-else:
-    print("a and b are equal b")
